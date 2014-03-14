b30e1da8339ed51ea077743d8a767cbbb85e22f9
Mouii <kevin.prudhomme555@gmail.com>
2014-03-15 00:20:40 +0100

    Change App

--- src/main/java/fr/univaix/iut/pokebattle/bot/App.java
+++
@@ -1,9 +0,0 @@
-package fr.univaix.iut.pokebattle.bot;
-
-import twitter4j.*;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
