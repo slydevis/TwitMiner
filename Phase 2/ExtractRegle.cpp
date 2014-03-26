dd7d3ae88d69cb37c7528d0a49571d2b9ab33a7b
Mouii <kevin.prudhomme555@gmail.com>
2014-03-26 13:24:52 +0100

    Algo de MinConf

---
+++ Phase 2/ExtractRegle.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Phase2.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "..." << endl; // prints ...
+	return 0;
+}
