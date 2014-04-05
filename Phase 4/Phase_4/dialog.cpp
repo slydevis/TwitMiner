c8df501eef48e1e33b779ea03758667e235ed7bc
Ragnos <slydevis@hotmail.fr>
2014-04-05 23:28:00 +0200

    Deuxieme commit P4

--- Phase 4/Phase_4/dialog.cpp
+++ Phase 4/Phase_4/dialog.cpp
@@ -59,7 +59,75 @@ QString getItems(QString str)
 
 QString getFreq(QString str, QString item)
 {
+    // On vérifie si on est bon si non on return ""
+
+    QString tmp;
+    for(int i = 0; i < str.size() - 1; ++i)
+    {
+        if(str.at(i + 1) == '(' || str.at(i) == '(') break;
+        else {
+            tmp.push_back(str.at(i));
+        }
+    }
+
+    // On extract la fréquence
+    if(tmp == item)
+    {
+        QString freq;
+        for(int i = 0; i < str.size() - 1; ++i)
+        {
+            if(str.at(i) == '(')
+            {
+                for(int j = i+1; i < str.size() - 1; ++j)
+                {
+                    freq    .push_back(str.at(j));
+                    if(str.at(j+1) == ')') break;
+                }
+                break;
+            }
+        }
+        return freq;
+    }
+    else return "";
     return str;
+}
+
+QString getLift(QString Item, QString Conf)
+{
+    // On construit l'Item cherché
+    QString ItemCherche;
+    bool remplir = false;
+    for(int i = 0; i < Item.size(); ++i)
+    {
+        if(Item.at(i) == ' ' && remplir == false)
+        {
+            remplir = true;
+            i++;
+        }
+        if(remplir) ItemCherche.push_back(Item.at(i));
+    }
+
+    // On cherche la fréquence associé
+
+    QString Freq;
+
+    QFile isLift("ressources.nette");
+    if(isLift.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream flux(&isLift);
+        while(!flux.atEnd())
+        {
+            QString tmp = flux.readLine();
+            Freq.push_back(getFreq(tmp, ItemCherche));
+        }
+        isLift.close();
+    }
+    QString Lift;
+    float lifting;
+    lifting = Freq.toFloat() * Conf.toFloat();
+    Lift = QString::number(lifting, 'f', 6);
+    return Lift;
+
 }
 
 Dialog::Dialog(QWidget *parent) :
@@ -70,11 +138,13 @@ Dialog::Dialog(QWidget *parent) :
 }
 
 void Dialog::createWindows() {
+
     // Init all tab
 
     QFile isAsso(this->cheminFicAsso);
     std::vector<QString> TabConf;
     std::vector<QString> TabItem;
+    std::vector<QString> TabLift;
     if(isAsso.open(QIODevice::ReadOnly | QIODevice::Text))
     {
         QTextStream flux(&isAsso);
@@ -99,10 +169,18 @@ void Dialog::createWindows() {
         {
             QString tmp = flux.readLine();
             for(unsigned i = 0; i < TabItem.size(); ++i)
-                TabFreq.push_back(getFreq(tmp, TabItem.at(i)));
+            {
+                if(getFreq(tmp, TabItem.at(i)) != "")
+                    TabFreq.push_back(getFreq(tmp, TabItem.at(i)));
+            }
         }
     }
     else std::cout << "Y a une erreur à l'ouverture du fichier" << std::endl;
+
+    for(unsigned i = 0; i < TabItem.size(); ++i)
+    {
+        TabLift.push_back(getLift(TabItem.at(i), TabConf.at(i)));
+    }
 
     // Création de la fenetre
 
@@ -111,15 +189,35 @@ void Dialog::createWindows() {
     model->setHorizontalHeaderItem(1, new QStandardItem(QString("Item")));
     model->setHorizontalHeaderItem(2, new QStandardItem(QString("Fréquence")));
     model->setHorizontalHeaderItem(3, new QStandardItem(QString("Lift")));
-    QString conf = TabConf.at(0);
-    QStandardItem *firstRow = new QStandardItem(conf);
-    QString item = TabItem.at(0);
-    QStandardItem *secondRow = new QStandardItem(item);
-    QString freq = TabFreq.at(0);
-    QStandardItem *thirdRow = new QStandardItem(freq);
-    model->setItem(0,0, firstRow);
-    model->setItem(0,1, secondRow);
-    model->setItem(0,2, thirdRow);
+
+    for(unsigned i = 0; i < TabConf.size(); ++i)
+    {
+        QString conf = TabConf.at(i);
+        QStandardItem *firstRow = new QStandardItem(conf);
+        model->setItem(i,0, firstRow);
+    }
+
+    for(unsigned i = 0; i < TabItem.size(); ++i)
+    {
+        QString item = TabItem.at(i);
+        QStandardItem *secondRow = new QStandardItem(item);
+        model->setItem(i,1, secondRow);
+    }
+
+    for(unsigned i = 0; i < TabFreq.size(); ++i)
+    {
+        QString freq = TabFreq.at(i);
+        QStandardItem *thirdRow = new QStandardItem(freq);
+        model->setItem(i,2, thirdRow);
+    }
+
+    for(unsigned i = 0; i < TabLift.size(); ++i)
+    {
+        QString lift = TabLift.at(i);
+        QStandardItem *fourthRow = new QStandardItem(lift);
+        model->setItem(i,3, fourthRow);
+    }
+
     ui->tableView->setModel(model);
     ui->tableView->setColumnWidth(0,100);
 }
